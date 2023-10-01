125480a1bfad711a5c843d07c1d767f67b6c26a8
laukic1 <dlauko96@gmail.com>
2023-10-01 09:22:58 +0200

    Finishing touches for the minor issues of the website

--- src/components/typewriter-effect/typewriter-effect.component.jsx
+++ src/components/typewriter-effect/typewriter-effect.component.jsx
@@ -7,7 +7,7 @@ const TypewriterEffect = () => {
         <span style={{ color: 'black', fontWeight: 'bold',}}>
           {/* Style will be inherited from the parent element */}
           <Typewriter
-            words={['building innovative and user-friendly applications.', 'providing smart solutions.', 'Always eager to learn new technologies.', 'Passionate about coding and problem-solving.']}
+            words={['building innovative and user-friendly applications.', 'providing smart solutions.', 'always eager to learn new technologies.', 'passionate about coding and problem-solving.']}
             loop={5}
             cursor
             cursorStyle='_'
